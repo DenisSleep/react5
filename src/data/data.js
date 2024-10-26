a7d6d718c271a3aaeaa11aa699289211d755b0a7
DenisSleep <160733810+DenisSleep@users.noreply.github.com>
2024-10-26 23:41:54 +0300

    Реакт 5

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+export let products = [
+                    {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/7952606/560x504/92357870.jpg",
+                        name: 'Product1',
+                        price: "12 312"
+ 
+                    }, {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/1023931/560x504/51468325.jpg",
+                        name: 'Product2',
+                        price: "2 342"
+                    }, {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/835326/560x504/51206002.jpg",
+                        name: 'Product3',
+                        price: "234"
+                    }, {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/2947220/560x504/57172898.jpg",
+                        name: 'Product4',
+                        price: "23 423"
+
+                    }, {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/1128608/560x504/51885879.jpg",
+                        name: 'Product5',
+                        price: "844"
+                
+                    }, {
+                        img: "https://cdn.vseinstrumenti.ru/images/goods/santehnicheskoe-oborudovanie/tovary-dlya-vannoj-komnaty-i-tualeta/11160260/560x504/150572090.jpg",
+                        name: 'Product6',
+                        price: "123 133"
+
+                    }
+                ];
